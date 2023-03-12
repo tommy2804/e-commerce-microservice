79e3397aea17c5b80f08c6170569815325c4a99e
tommy2804 <t1o2mm3y@gmail.com>
2023-03-12 21:24:05 +0200

    add startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,8 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 mongoose.set('strictQuery', false);
 
 const start = async () => {
+  console.log('Starting....');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
